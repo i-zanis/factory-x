5194f7b56203da3c3c1261e93537e85f09ee579f
i-zanis <ioanlafazanis@gmail.com>
2023-12-04 20:18:31 +0000

    feat(eureka): remove security

---
+++ product-eureka/src/main/java/com/izanis/producteureka/ProductEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.izanis.producteureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ProductEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductEurekaApplication.class, args);
+	}
+
+}
---
+++ product-eureka/src/test/java/com/izanis/producteureka/ProductEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.izanis.producteureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
