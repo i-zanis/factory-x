a8f64bccc6fa386efa4274f06309a40ac66309eb
i-zanis <ioanlafazanis@gmail.com>
2023-12-07 21:17:05 +0000

    chore(config-server): update application properties

--- config-server/src/main/java/com/izanis/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/izanis/configserver/ConfigServerApplication.java
@@ -2,12 +2,15 @@ package com.izanis.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
-	}
 
+  //TODO(i-zanis): add configuration for all services (local profile), Posgreqsl
+  public static void main(String[] args) {
+    SpringApplication.run(ConfigServerApplication.class, args);
+  }
 }
