fedb589385c59ad3959089743aae5480335cef45
i-zanis <ioanlafazanis@gmail.com>
2023-12-11 20:03:59 +0000

    chore(inventory-service) add logback configuration

---
+++ product-inventory-service/src/main/java/com/izanis/productinventoryservice/config/LocalDiscovery.java
@@ -0,0 +1,10 @@
+package com.izanis.productinventoryservice.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@EnableDiscoveryClient
+@Profile("local-discovery")
+public class LocalDiscovery {}
