ef492d98e2fbc1a4d53efd827a2a0a446b6497b0
i-zanis <ioanlafazanis@gmail.com>
2025-07-01 23:39:00 +0100

    remove outdated architectural parts

--- product-inventory-failover/src/main/java/com/izanis/productinventoryfailover/model/ProductInventoryDto.java
+++
@@ -1,5 +0,0 @@
-package com.izanis.productinventoryfailover.model;
-
-import java.util.List;
-
-public record ProductInventoryDto(List<ProductInventoryItem> items) {}
--- product-inventory-failover/src/main/java/com/izanis/productinventoryfailover/model/ProductInventoryItem.java
+++
@@ -1,5 +0,0 @@
-package com.izanis.productinventoryfailover.model;
-
-import java.util.UUID;
-
-public record ProductInventoryItem(UUID id, Integer inventory) {}
