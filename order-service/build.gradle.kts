8658b0a3218d0560c55a4b746d4faef0da36bac6
i-zanis <ioanlafazanis@gmail.com>
2026-04-06 20:49:01 +0100

    update build.gradle

--- order-service/build.gradle.kts
+++ order-service/build.gradle.kts
@@ -1,10 +1,54 @@
 plugins {
     id("java")
     id("org.springframework.boot")
+    id("com.google.protobuf") version "0.9.4"
 }
 
 dependencies {
+    implementation(project(":common-domain"))
+    implementation(project(":catalog-service"))
+
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("org.postgresql:postgresql")
+
+    // [gRPC Client]
+    implementation("net.devh:grpc-spring-boot-starter:3.1.0")
+    implementation("io.grpc:grpc-stub:1.69.0")
+    implementation("io.grpc:grpc-protobuf:1.69.0")
+    implementation("javax.annotation:javax.annotation-api:1.3.2")
+
+    // [Lombok]
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.25.5"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.69.0"
+        }
+    }
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                id("grpc")
+            }
+        }
+    }
+}
+
+sourceSets {
+    main {
+        java {
+            srcDir("$buildDir/generated/source/proto/main/java")
+            srcDir("$buildDir/generated/source/proto/main/grpc")
+        }
+    }
 }
