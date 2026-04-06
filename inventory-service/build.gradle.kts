a02e8cf9c1f4178b81543fd9ebab4ea22b307b38
i-zanis <ioanlafazanis@gmail.com>
2026-04-06 17:34:47 +0100

    update inventory-service

---
+++ inventory-service/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("org.springframework.boot")
+    kotlin("jvm")
+    kotlin("plugin.spring")
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
