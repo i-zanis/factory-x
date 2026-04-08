ecae5460bde408d83044dc94f7c897075fdf3324
i-zanis <ioanlafazanis@gmail.com>
2026-04-08 23:46:39 +0100

    add AuditInfo

---
+++ common-domain/src/main/java/com/factoryx/common/domain/AuditInfo.java
@@ -0,0 +1,34 @@
+package com.factoryx.common.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.Instant;
+
+@Embeddable
+@Getter
+@Setter
+public class AuditInfo {
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    @CreatedBy
+    @Column(name = "created_by", updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    @Column(name = "updated_by")
+    private String updatedBy;
+}
