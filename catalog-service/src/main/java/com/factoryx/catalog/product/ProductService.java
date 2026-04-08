2ceba9af4a705fad791f71f4d7c20c962dc706ab
i-zanis <ioanlafazanis@gmail.com>
2026-04-08 23:45:39 +0100

    add Product-related classes

---
+++ catalog-service/src/main/java/com/factoryx/catalog/product/ProductService.java
@@ -0,0 +1,35 @@
+package com.factoryx.catalog.product;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public List<ProductEntity> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Optional<ProductEntity> getProductById(UUID id) {
+        return productRepository.findById(id);
+    }
+    
+    public Optional<ProductEntity> getProductBySku(String sku) {
+        return productRepository.findBySku(sku);
+    }
+
+    @Transactional
+    public ProductEntity createProduct(String sku, String name, double price) {
+        ProductEntity newProduct = new ProductEntity(UUID.randomUUID(), sku, name, price);
+        return productRepository.save(newProduct);
+    }
+}
