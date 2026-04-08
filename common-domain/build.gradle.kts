f207df9048e6379ea0e62cd984b4779b3e96ec35
i-zanis <ioanlafazanis@gmail.com>
2026-04-08 23:47:00 +0100

    add Lombok

---
+++ common-domain/build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    id("java")
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+}
