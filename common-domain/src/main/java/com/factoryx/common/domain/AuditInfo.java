3ec96c7e3580348ecdc20653b0fc8e1565271e8f
i-zanis <ioanlafazanis@gmail.com>
2026-04-14 21:05:19 +0100

    enhance AuditInfo with additional methods and improve field accessibility

--- common-domain/src/main/java/com/factoryx/common/domain/AuditInfo.java
+++ common-domain/src/main/java/com/factoryx/common/domain/AuditInfo.java
@@ -2,18 +2,23 @@ package com.factoryx.common.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.Version;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.lang.NonNull;
 
+import java.time.Duration;
 import java.time.Instant;
 
 @Embeddable
 @Getter
 @Setter
 @NoArgsConstructor
-// TODO(i-zanis): think if this should be a record or will cause confusion
 public class AuditInfo {
 
     @CreatedDate
@@ -35,54 +40,46 @@ public class AuditInfo {
 
     @Column(updatable = false)
     private String deletedBy;
-    // TODO(i-zanis): see if this is the right import
+    
     @Version
     private Integer version;
 
-    // TODO(i-zanis): need to see if there are better names
-    void touch() {
+    public void touch() {
         updatedAt = Instant.now();
     }
 
-    // TODO(i-zanis): to test if this is correct
-    boolean isNew() {
+    public boolean isNew() {
         return version == null;
     }
 
-    // TODO(i-zanis): to check if these in records should be better as fields
-    boolean isDeleted() {
+    public boolean isDeleted() {
         return deletedAt != null;
     }
 
-    boolean markDeleted() {
+    public boolean markDeleted(String user) {
         if (isDeleted()) return false;
         deletedAt = Instant.now();
-        // TODO(i-zanis): find the proper way to get the user with Spring Security
-        deletedBy = "user";
+        deletedBy = user;
         return true;
     }
 
-    boolean isCreatedBefore(Instant instant) {
+    public boolean isCreatedBefore(@NonNull Instant instant) {
+        if (createdAt == null) return false;
         return createdAt.isBefore(instant);
     }
 
-    boolean isCreatedAfter(Instant instant) {
+    public boolean isCreatedAfter(@NonNull Instant instant) {
+        if (createdAt == null) return false;
         return createdAt.isAfter(instant);
     }
 
-    Instant timeSinceCreation() {
-        return Instant.now().difference(createdAt);
+    public Duration timeSinceCreation() {
+        if (createdAt == null) return Duration.ZERO;
+        return Duration.between(createdAt, Instant.now());
     }
 
-    Instant timeSinceLastUpdate() {
-        return Instant.now().difference(updatedAt);
+    public Duration timeSinceLastUpdate() {
+        if (updatedAt == null) return Duration.ZERO;
+        return Duration.between(updatedAt, Instant.now());
     }
-
-    // TODO(i-zanis): would this be useful here?
-    String anonymize() {
-        return "";
-    }
-
-    // TODO(i-zanis): check if there are other Automatic fields like @LastMOdifiedBy
-
 }
